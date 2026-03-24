713ad656099fafb628a8264d09fdbe8531d18168
henhen <nkht0k4n@gmail.com>
2026-03-24 16:07:31 +0900

    first commit

---
+++ src/main.rs
@@ -0,0 +1,137 @@
+use clap::Parser;
+use std::fs::File;
+use std::io::{stdin,BufRead,BufReader};
+use anyhow::{bail,ensure,Context,Result};
+use std::path::PathBuf;
+
+#[derive(Parser,Debug)]
+#[clap(
+    name="My RPN program",
+    version="1.0.0",
+    author="Your name",
+    about="Super awesome sample RPN calculator"
+
+)]
+
+struct Opts{
+
+    #[clap(short,long)]/*オプション用*/
+    verbose:bool,
+
+    #[clap(name="FILE")]/*単なる引数用*/
+    formula_file: Option<PathBuf>,
+}
+
+struct RpnCalculator(bool);
+
+impl RpnCalculator{
+pub fn new(verbose:bool)->Self{
+    Self(verbose)
+}
+
+pub fn eval(& self,formula: &str)-> Result<i32>{
+    let mut tokens=formula.split_whitespace().rev().collect::<Vec<_>>();
+    self.eval_inner(&mut tokens)
+}
+fn eval_inner(&self,tokens:&mut Vec<&str>)->Result<i32>{
+    let mut stack =Vec::new();
+    let mut pos=0;
+
+    while let Some(token)=tokens.pop(){
+        pos+=1;
+        if let Ok(x)=token.parse::<i32>(){
+            stack.push(x)
+        }else{
+            let y=stack.pop().with_context(||format!("invalid syntax at {}",pos))?;
+            let x=stack.pop().with_context(||format!("invalid syntax at {}",pos))?;
+
+            let res=match token{
+                "+"=>x+y,
+                "-"=>x-y,
+                "*"=>x*y,
+                "/"=>x/y,
+                "%"=>x%y,
+                _=>bail!("invalid token at {}",pos),
+            };
+            stack.push(res);
+        }
+
+        if self.0{
+            println!("{:?} {:?}",tokens,stack);
+        }
+    }
+    ensure!(stack.len()==1,"invalid syntax");
+    Ok(stack[0])
+
+}
+
+
+}
+
+
+
+fn main()->Result<()>{
+    let opts=Opts::parse();
+
+    if let Some(path)=opts.formula_file{
+        let f=File::open(path)?;/*ファイルを開き,成功したらファイルオブジェクトをfに入れて失敗したらプログラムを停止*/
+        let reader=BufReader::new(f);
+        run(reader,opts.verbose)
+    }else{
+        let stdin=stdin();
+        let reader=stdin.lock();
+        run(reader,opts.verbose)
+    }
+
+    }
+
+
+fn run<R:BufRead>(reader: R,verbose:bool)->Result<()>{
+
+    let calc=RpnCalculator::new(verbose);
+
+    for line in reader.lines(){
+        let line=line?;
+        match calc.eval(&line){
+            Ok(answer)=>println!("{}",answer),
+            Err(e)=>eprintln!("{:#?}",e),
+        }
+    }
+    Ok(())
+}
+  
+
+#[cfg(test)]
+mod tests{
+    use super::*;
+
+    #[test]
+    fn test_ok(){
+        let calc=RpnCalculator::new(false);
+        assert_eq!(calc.eval("5").unwrap(),5);
+        assert_eq!(calc.eval("50").unwrap(),50);
+        assert_eq!(calc.eval("-50").unwrap(),-50);
+
+        assert_eq!(calc.eval("2 3 +").unwrap(),5);
+        assert_eq!(calc.eval("2 3 *").unwrap(),6);
+        assert_eq!(calc.eval("2 3 -").unwrap(),-1);
+        assert_eq!(calc.eval("2 3 /").unwrap(),0);
+        assert_eq!(calc.eval("2 3 %").unwrap(),2);
+
+    }
+
+    #[test]
+    fn test2_ok(){
+        let calc=RpnCalculator::new(false);
+        assert_eq!(calc.eval("5").unwrap(),5);
+        assert_eq!(calc.eval("50").unwrap(),50);
+        assert_eq!(calc.eval("-50").unwrap(),-50);
+
+        assert_eq!(calc.eval("2 3 +").unwrap(),5);
+        assert_eq!(calc.eval("2 3 *").unwrap(),6);
+        assert_eq!(calc.eval("2 3 -").unwrap(),-1);
+        assert_eq!(calc.eval("2 3 /").unwrap(),0);
+        assert_eq!(calc.eval("2 3 %").unwrap(),2);
+
+    }
+}
